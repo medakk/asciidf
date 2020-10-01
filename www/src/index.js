652cea440bdf279cd0175ee04f1c5cb641c60373
Karthik Karanth <karanth.karthik@gmail.com>
2020-10-01 02:11:51 -0600

    line height thing

--- www/src/index.js
+++ www/src/index.js
@@ -4,5 +4,6 @@ import { do_thing } from 'asciidf';
 const s = do_thing();
 
 const pre = document.createElement('pre');
+pre.style.lineHeight = 0.9;
 pre.innerHTML = s;
 document.body.appendChild(pre);
